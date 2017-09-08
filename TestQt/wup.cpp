a3824f2cb6d201c3885c9e4d8bd3a959007962f5
ehodne <espen1905@gmail.com>
2017-09-08 12:02:12 +0200

    stuff?

--- TestQt/wup.cpp
+++ TestQt/wup.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
+
+std::ofstream ofile;
 
 double f(double x){
     double arc = atan(x);
@@ -10,18 +13,29 @@ double f(double x){
 }
 
 int main(){
-    double x = sqrt(4);
-    int N = 100;
-    double h[100];
+    double x = sqrt(2.0);
+    int N = 10000;
+    double h[N];
+    double fder2c[N];
+    double fder3c[N];
+    double epsilon2c[N];
+    double epsilon3c[N];
+    int i;
 
     for(i=1; i<N; i++){
-        h[i] = 1.0/i
+        h[i] = 1.0/(i*10);
+        fder2c[i] = (f(x + h[i]) - f(x))/h[i];
+        fder3c[i] = (f(x + h[i]) - f(x - h[i]))/(2*h[i]);
+
+        epsilon2c[i] = log(abs( (fder2c[i] - 1.0/3)/(1.0/3) ));
+        epsilon3c[i] = log(abs( (fder3c[i] - 1.0/3)/(1.0/3) ));
+    }
+
+    ofile.open("errors.txt", std::ios::out);
+    for (i=1; i<N; i++){
+        ofile << h[i] << " " << fder2c[i] << " " << fder3c[i] << " " << epsilon2c[i] << " " << epsilon3c[i] << endl;
 
     }
-    double fder2c = (f(x + h) - f(x))/h;
-    double fder3c = (f(x + h) - f(x - h))/(2*h);
-
-    cout << fder2c << " " << fder3c << endl;
+    ofile.close();
     return 0;
 }
-
--- project1/project1_specialized/main.cpp
+++ project1/project1_specialized/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <armadillo>
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 using namespace arma;
@@ -11,15 +12,9 @@ int main(int argc, char** argv)
 
     int flops = 0;  //floating point operator counter
     int n;          //matrix and array size
-    double D;       //main diagonal numbers
-    double E;       //secondary diagonal numbers
     double h;       //step length
     n = atoi(argv[1]);
-    D = atoi(argv[2]);
-    E = atoi(argv[3]);
 
-    vec d(n);       //vector containing main diagonal numbers
-    vec e(n-1);     //vector containing secondary diagonal numbers
     vec x;          //integration area
     vec f(n);       //known function
     vec d_tilde(n); //modified main diagonal
@@ -32,42 +27,58 @@ int main(int argc, char** argv)
 
     f = h*h*100*exp(-10*x);
     flops += 4;
-    d.fill(D);
-    e.fill(E);
 
     u(0) = 0;
     u(n-1) = 0;
-    d_tilde[0] = d[0];
+    d_tilde[0] = 2;
     f_tilde[0] = f[0];
 
-    for(int i=1; i<n; i++){     //forwards substitution
-        d_tilde[i] = d[i] - (e[i-1]*e[i-1])/d_tilde[i-1];
-        flops += 3;
-        f_tilde[i] = f[i] - (e[i-1]*f_tilde[i-1])/d_tilde[i-1];
-        flops += 3;
+    vec cfs(n);
+    cfs = 1 - (1-exp(-10))*x - exp(-10*x);
+
+    for(double i=1; i<n; i++){     //forwards substitution
+        d_tilde[i] = (i+1)/i;
+        flops += 2;
+        f_tilde[i] = f[i] + f_tilde[i-1]/d_tilde[i-1];
+        flops += 2;
     }
 
     u(n-2) = f_tilde(n-2)/d_tilde(n-2);
-    flops += 3;
+    flops += 1;
     for(int i=n-2; i>0; i--){   //backwards substitution
-        u(i) = (f_tilde[i] - e[i]*u[i+1])/d_tilde[i];
-        flops += 4;
+        u(i) = (f_tilde[i] + u[i+1])/d_tilde[i-1];
+        flops += 2;
     }
 
-    vec cfs(n);
+    vec error(n);
+    error = log10( abs( (u - cfs)/cfs ) );
+    error[0] = error[1];
+    //cout << error;
+
+    mat A(n,n);
+    A(0,0) = 2;
+
     for(int i=1; i<n; i++){
-        cfs[i] = 1 - (1-exp(-10))*x[i] - exp(-10*x[i]);
+        A(i,i) = 2;
+        A(i,i-1) = -1;
+        A(i-1,i) = -1;
     }
+    cout << A << endl;
+
+
+
     ofstream myfile;
-    string name = "pls" + to_string(n) + ".txt";
+    string name = "pls_special" + to_string(n) + ".txt";
     myfile.open(name);
 
     for(int i = 0; i<n; i++){
-        myfile << x[i] << "\t" << u[i] << "\t" << cfs[i] << "\n";
+        myfile << x[i] << "\t" << u[i] << "\t" << cfs[i] << "\t" << error[i] << "\n";
     }
 
     myfile.close();
+
     cout << "Program run time: "<<(clock()-start)/float(CLOCKS_PER_SEC)<< " seconds" << endl;
 
+    cout << "FLOPS:" << flops;
     return 0;
 }
